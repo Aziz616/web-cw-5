d050ec057bdd24718b18d20e9ca25926bfb5183d
Aziz616 <alwazzan157@gmail.com>
2022-03-16 19:35:55 +0300

    خلصت

--- java.js
+++ java.js
@@ -3,3 +3,9 @@ console.log("عبدالعزيز الوزان");
 console.log("انا في الصف العاشر");
 console.log("%cHello", "color: green; font-size: 30px");
 // hgg:
+
+let budget = 20;
+console.log(budget > 30 && budget < 80);
+
+budget += 30;
+console.log(budget > 30 && budget < 80);
